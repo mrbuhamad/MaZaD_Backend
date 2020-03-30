2f6824cc95b88c73554e9d80a66dd89fef79fc34
mrbuhamad <mr.buhamad@hotmail.com>
2020-03-30 19:22:14 +0300

    Users Model Set up

--- API/models.py
+++ API/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Bidder(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.name
+
+
+class Vender(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.name
