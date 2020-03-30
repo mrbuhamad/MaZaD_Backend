e80037d2564de2fcb16d7618c283e6db71b2bf4a
mrbuhamad <mr.buhamad@hotmail.com>
2020-03-30 19:59:34 +0300

    Adress and Categori models added

--- API/models.py
+++ API/models.py
@@ -14,3 +14,23 @@ class Vender(models.Model):
 
     def __str__(self):
         return self.user.name
+
+
+class Addres(models.Model):
+    country = models.CharField(max_length=120)
+    area=models.CharField(max_length=120)
+    block=models.CharField(max_length=120)
+    street=models.CharField(max_length=120)
+    house=models.CharField(max_length=120)
+    bidder = models.ForeignKey(Bidder, on_delete=models.CASCADE,null=True)
+    vender = models.ForeignKey(Vender, on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return f"{self.area} {self.block} {self.street}"
+
+
+class Categori(models.Model):
+    type = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.type
