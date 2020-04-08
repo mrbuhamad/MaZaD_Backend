1ff5749560016d741799bfbca118406748a6543b
mrbuhamad <mr.buhamad@hotmail.com>
2020-04-08 04:19:49 +0300

    permission added (no bidding permition yet)

--- API/permissions.py
+++ API/permissions.py
@@ -1,6 +1,10 @@
 from rest_framework.permissions import BasePermission
 
-	
+#  Models
+from .models import *
+
+# Serializers
+from .serializers import *
 
 class IsVender(BasePermission):
 	message = "user have to be a Vender"
@@ -8,24 +12,49 @@ class IsVender(BasePermission):
 	def  has_permission(self, request, view):
 		if request.user.is_staff or hasattr(request.user, 'vender'):
 			return True
+		return False
 
+
+class CanCreateItem(BasePermission):
+	message = "user have to be a AuctionOwner Vender"
+
+	def has_permission(self, request, view):
+		user_id = getattr(request.user, 'id')
+		data =request.data['items']
+		auction_id = data[0]['auction']
+		print(auction_id)
+		if auction_id is not None:
+			auction_obj = Auction.objects.get(id=auction_id)
+			return user_id == auction_obj.vender.user.id
 		return False
 
 class IsAuctionOwner(BasePermission):
-	message = "user have to be a Vender"
+	message = "user have to be a AuctionOwner Vender"
+
+
 
 	def has_object_permission(self, request, view, obj):
-		if request.user.is_staff or (obj.user.vender == request.user.vender):
+		if request.user.is_staff or (obj.vender.user == request.user):
 			return True
 
 		return False
 
+class IsItemOwner(BasePermission):
+	message = "user have to be a Item Owner"
+
+	def has_object_permission(self, request, view, obj):
+		if request.user.is_staff or (obj.auction.vender.user == request.user):
+			return True
+
+		return False
 
 class IsVarifideBidder (BasePermission):
 	message = "bidder have to provide a varified pyment info"
 
-	def has_object_permission(self, request, view, obj):
-		if request.user.is_staff or obj.user.participant.exists():
-			return True
 
+	def  has_permission(self, request, view):
+		if request.user.is_staff or hasattr(request.user.bidder, 'participant'):
+			return True
 		return False
+
+	
