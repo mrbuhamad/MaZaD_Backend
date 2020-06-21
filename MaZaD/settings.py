1b244ed0386fd4da92eeb338d1a056258e9cabfa
mrbuhamad <mr.buhamad@hotmail.com>
2020-06-21 18:34:35 +0300

    spilling mistake and icon changed

--- MaZaD/settings.py
+++ MaZaD/settings.py
@@ -145,13 +145,20 @@ USE_TZ = True
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'go.livemazad@gmail.com'
-EMAIL_HOST_PASSWORD = 'mazad2020'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'go.livemazad@gmail.com'
+# EMAIL_HOST_PASSWORD = 'mazad2020'
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_HOST_USER = 'livemazad@outlook.com'
+EMAIL_HOST_PASSWORD = 'mazad2020'
+EMAIL_PORT = 25
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
